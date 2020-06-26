e6983a0ba5526e5d9d0dd01c4ea09d4fc26b0dca
Tyler Edwards <tredwards2002@gmail.com>
2020-06-25 23:45:27 -0400

    removed dist and node modules folder from repository

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const csv: any;
-declare const fs: any;
-declare const levenshtein: any;
-declare var results: any[];
-declare function argsort(clickCount: number[]): number[];
-declare function string_comparison(s1: any, s2: any): number;
-declare function get_dists(test: string, res: number[], index: string): number[];
-declare const man_test = "Beck Coulter";
-declare const mod_test = "Act5";
